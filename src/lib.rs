d8e6b58129dd8ce095235f9d08c5777002498954
Franciszek Piszcz <franek.piszcz@gmail.com>
2023-05-29 20:47:59 +0200

    Copy the impl crate from original linkme

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,6 @@ mod distributed_fn_slice;
 #[path = "private.rs"]
 pub mod __private;
 
-//pub use generic_linkme_impl::*;
+pub use generic_linkme_impl::*;
 
 pub use crate::distributed_fn_slice::DistributedFnSlice;
